dad8bdeeb86620b184311316647889f5370e63a2
Pavel Neyman <tushqan1@mail.ru>
2017-10-31 19:36:54 +0700

    Добавлена компонет привязки префаба с тайлом к сетке.

---
+++ Assets/Scripts/SnapperComponent.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using UnityEngine;
+
+/// <summary>
+/// Компонент отвечающий за привязку координат к сетке.
+/// </summary>
+/// <remarks>
+/// Выполняется в редакторе.
+/// </remarks>
+[ExecuteInEditMode]
+public class SnapperComponent : MonoBehaviour {
+#if UNITY_EDITOR
+
+    /// <summary>
+    /// Компонент включен.
+    /// </summary>
+    [DefaultValue(true)]
+    public bool IsEnabled;
+
+    /// <summary>
+    /// Величена ячейки сетки.
+    /// </summary>
+    [DefaultValue(1)]
+    public float CellSize = 1;
+
+    private void Update()
+    {
+        if (IsEnabled)
+            transform.position = GetSnappedPosition(transform.position, CellSize);
+    }
+
+    /// <summary>
+    /// Возвращает выравненную позицию объекта.
+    /// </summary>
+    /// <param name="position">Позиция объекта.</param>
+    /// <param name="cellSize">Размер ячейки.</param>
+    private Vector3 GetSnappedPosition(Vector3 position, float cellSize)
+    {
+        return new Vector3
+        (
+            cellSize * Mathf.Round(position.x / cellSize),
+            0,
+            cellSize * Mathf.Round(position.z / cellSize)
+        );
+    }
+#endif
+}
